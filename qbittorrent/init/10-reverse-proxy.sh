139f044a1b53115364c02ba94642408474b890d4
RaylenZed <raylenzed@gmail.com>
2026-02-15 02:38:11 +0800

    Stabilize qB behind Caddy by simplifying proxy headers

--- qbittorrent/init/10-reverse-proxy.sh
+++ qbittorrent/init/10-reverse-proxy.sh
@@ -30,7 +30,7 @@ set_pref 'WebUI\Address' '0.0.0.0'
 set_pref 'WebUI\Port' '8080'
 set_pref 'WebUI\HostHeaderValidation' 'false'
 set_pref 'WebUI\CSRFProtection' 'false'
-set_pref 'WebUI\ReverseProxySupportEnabled' 'true'
+set_pref 'WebUI\ReverseProxySupportEnabled' 'false'
 set_pref 'WebUI\ServerDomains' "${BASE_DOMAIN};${BASE_DOMAIN}:${QBIT_HTTPS_PORT};localhost;127.0.0.1"
 set_pref 'WebUI\TrustedReverseProxies' '127.0.0.1/8;10.0.0.0/8;172.16.0.0/12;192.168.0.0/16;fc00::/7'
 set_pref 'WebUI\AlternativeUIEnabled' 'false'
